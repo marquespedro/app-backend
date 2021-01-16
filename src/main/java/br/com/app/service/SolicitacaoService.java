12dc27b1b4d957e959131e41256ffdf760e6c5e9
Pedro Ivo <ivo.mrqs@gmail.com>
2021-01-16 15:40:49 -0300

    ajuste metodo para retornar strategy.

--- src/main/java/br/com/app/service/SolicitacaoService.java
+++ src/main/java/br/com/app/service/SolicitacaoService.java
@@ -1,9 +1,14 @@
 package br.com.app.service;
 
+import java.util.Objects;
+
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
+import br.com.app.exception.AppException;
+import br.com.app.exception.MensagemErro;
 import br.com.app.model.Solicitacao;
+import br.com.app.model.enumerator.TipoSolicitacaoEnum;
 import br.com.app.service.strategy.SolicitacaoStrategy;
 import br.com.app.strategy.SolicitacaoStrategyFactory;
 
@@ -14,8 +19,19 @@ public class SolicitacaoService {
 	private SolicitacaoStrategyFactory strategyFactory;
 
 	public Solicitacao criar(Solicitacao solicitacao) {
-		SolicitacaoStrategy strategy = strategyFactory.getStrategy(solicitacao.getTipo().getCodigo());
-		return strategy.criar(solicitacao);
+			
+		return  obterStrategy(solicitacao).criar(solicitacao);
+	}
+
+	private SolicitacaoStrategy obterStrategy(Solicitacao solicitacao) {
+		
+		TipoSolicitacaoEnum tipo = TipoSolicitacaoEnum.comoEnum(solicitacao.getTipo().getCodigo());
+		
+		if(Objects.isNull(tipo)) {
+			throw new AppException(new MensagemErro("tipo.solicitacao.invalido", solicitacao.getTipo().getCodigo().toString()));
+		}
+		
+		return strategyFactory.getStrategy(tipo.getCodigo());
 	}
 
 	
