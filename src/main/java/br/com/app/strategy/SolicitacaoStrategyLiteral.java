a9dcd672f34695f5e8ef0086caaedde5255dde35
Pedro Ivo <ivo.mrqs@gmail.com>
2021-01-16 15:40:22 -0300

    suprimindo warning.

--- src/main/java/br/com/app/strategy/SolicitacaoStrategyLiteral.java
+++ src/main/java/br/com/app/strategy/SolicitacaoStrategyLiteral.java
@@ -5,7 +5,7 @@ import javax.enterprise.util.AnnotationLiteral;
 import br.com.app.model.enumerator.TipoSolicitacaoEnum;
 import br.com.app.strategy.annotation.StrategySolicitacao;
 
-
+@SuppressWarnings(value = "all")
 public class SolicitacaoStrategyLiteral extends AnnotationLiteral<StrategySolicitacao> implements StrategySolicitacao{
 
 	private static final long serialVersionUID = 1L;
